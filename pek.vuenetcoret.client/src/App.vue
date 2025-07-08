3bdb6d6faa741c83f278ee8ebb8593d73dc5091e
IOT8 <qcjxberin@outlook.com>
2025-07-08 21:17:25 +0800

    重构 App.vue，更新样式和图标资源
    在 `App.vue` 中进行了大规模重构，替换为新的路由视图结构，添加全局样式引入，并修改了组件的生命周期钩子函数。
    移除了 `base.css` 和 `main.css` 中的原有样式规则，简化样式管理。
    添加了新的图像数据到 `logo.png`、`error-img.png`、`favicon.ico` 和 `01.jpg`，更新了图标和错误图标。
    在 `common.less` 中添加了全局样式，支持新的布局和组件样式。
    新增了 `common.js`、`extend.js` 和 `testFormExtend.js` 中的函数，扩展了 Vue 参数和功能。

--- pek.vuenetcoret.client/src/App.vue
+++ pek.vuenetcoret.client/src/App.vue
@@ -1,47 +1,73 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  import "./assets/css/common.less";
+  export default {
+    name: "App",
+    created() {
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+    },
+    mounted() {
+      document.getElementById("v-loading-container").style.display = "none";
+    }
+  };
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  .el-loading {
+    z-index: 999999;
+  }
+  /* 解决element-ui的table表格控件表头与内容列不对齐问题 */
+  /* .el-table th.gutter{
+    display: table-cell !important;
+  } */
+  .el-table th {
+    display: table-cell !important;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .el-loading .el-loading-spinner {
+    padding: 7px;
+    background: #ececec;
+    width: 200px;
+    color: red;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    border-radius: 5px;
+    border: 1px solid #a0a0a0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+    /* margin-top: 60px; */
   }
-}
+
+  #nprogress .bar {
+    background: white;
+    height: 2px;
+    /* filter: blur(0.5px) */
+  }
+
+  .ivu-form .ivu-form-item-label {
+    color: #2e2e2f;
+  }
+  /* 2021.03.20修复明细表格错位 */
+  .grid-detail .el-table__fixed-body-wrapper {
+    top: 41px !important;
+  }
+</style>
+<style>
+  html,
+  body,
+  #app {
+    height: 100%;
+  }
 </style>
