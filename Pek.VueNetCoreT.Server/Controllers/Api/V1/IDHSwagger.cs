130700714842cf8e7ecf9f2867659d1ecb3b6537
猿人易 <qcjxberin@outlook.com>
2025-07-09 12:25:08 +0800

    添加 Versions 类和 MenuController 控制器
    在 `IDHSwagger.cs` 中添加 `Versions` 类，实现 `IDHSwagger` 接口，包含 `ApiVersions` 和 `Description` 属性。
    在 `MenuController.cs` 中引入必要的命名空间，定义 `MenuController` 控制器，使用 JWT 授权，并实现 `Index` 方法返回视图。

---
+++ Pek.VueNetCoreT.Server/Controllers/Api/V1/IDHSwagger.cs
@@ -0,0 +1,11 @@
+﻿using Pek.Swagger;
+
+namespace Pek.VueNetCoreT.Server.Controllers.Api.V1;
+
+public class Versions : IDHSwagger
+{
+
+    public ApiVersions ApiVersions { get; set; } = ApiVersions.V1;
+
+    public String Description { get; set; } = "<br /><strong>备注:</strong> 创建于2025-07-09。";
+}
---
+++ Pek.VueNetCoreT.Server/Controllers/Api/V1/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using Pek.NCube;
+
+namespace Pek.VueNetCoreT.Server.Controllers.Api.V1;
+
+[Authorize("jwt")]
+public class MenuController : PekControllerBaseX
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
