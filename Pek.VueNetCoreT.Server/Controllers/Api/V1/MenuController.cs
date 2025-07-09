090ef6079436470ea18f3e23cbf12c0fe3082c5d
猿人易 <qcjxberin@outlook.com>
2025-07-09 12:30:05 +0800

    更新 MenuController.cs，重构菜单接口
    在 `MenuController.cs` 文件中，移除了对 `Microsoft.AspNetCore.Authorization` 的引用，并添加了对 `Microsoft.AspNetCore.Mvc` 和其他几个命名空间的引用。
    添加了类的 XML 注释，描述了该类的功能和创建时间。
    将原有的 `Index` 方法替换为 `GetTreeMenu` 方法，并添加了 HTTP GET 和 POST 路由。
    在 `GetTreeMenu` 方法中，创建了一个 `DGResult` 对象，并设置其状态码为 `StateCode.Ok`，然后返回该结果。

--- Pek.VueNetCoreT.Server/Controllers/Api/V1/MenuController.cs
+++ Pek.VueNetCoreT.Server/Controllers/Api/V1/MenuController.cs
@@ -1,15 +1,31 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
+using Pek.Helpers;
+using Pek.Models;
+using Pek.MVC;
 using Pek.NCube;
+using Pek.Permissions;
+using Pek.Swagger;
 
 namespace Pek.VueNetCoreT.Server.Controllers.Api.V1;
 
-[Authorize("jwt")]
+/// <summary>
+/// 菜单接口
+/// </summary>
+/// <remarks>
+/// 建立时间: 2025-07-09
+/// </remarks>
+[Produces("application/json")]
+[CustomRoute(ApiVersions.V1, PrefixName: "")]
+[JwtAuthorize("Vue")]
 public class MenuController : PekControllerBaseX
 {
-    public IActionResult Index()
+    [HttpGet, HttpPost, Route("GetTreeMenu")]
+    public IActionResult GetTreeMenu()
     {
-        return View();
+        var result = new DGResult();
+
+        result.Code = StateCode.Ok;
+        return result;
     }
 }
