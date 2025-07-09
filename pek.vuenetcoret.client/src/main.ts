49d7c574ce9671dc7713cac11422b614b07f9ea7
廖庆龙 <2441830838@qq.com>
2025-07-09 11:09:01 +0800

    添加登录页面

--- pek.vuenetcoret.client/src/main.ts
+++ pek.vuenetcoret.client/src/main.ts
@@ -5,23 +5,23 @@ import store from './store'
 import ElementPlus from 'element-plus'
 // import 'element-plus/lib/theme-chalk/index.css';
 import 'element-plus/dist/index.css'
-import './assets/element-icon/icon.css'
-import base from './uitils/common'
-import http from './api/http'
+// import './assets/element-icon/icon.css'
+// import base from './uitils/common'
+// import http from './api/http'
 // import 'dayjs/locale/zh-cn'
 // import locale from 'element-plus/lib/locale/lang/zh-cn'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-import permission from './api/permission'
-import viewgird from './components/basic/ViewGrid'
+// import permission from './api/permission'
+// import viewgird from './components/basic/ViewGrid'
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-app.config.globalProperties.base = base
-app.config.globalProperties.http = http
+// app.config.globalProperties.base = base
+// app.config.globalProperties.http = http
 app.config.globalProperties.$tabs = {}
-app.config.globalProperties.permission = permission
+// app.config.globalProperties.permission = permission
 app.config.globalProperties.$global = {
   signalR: false, //是否开启signalR
   menuSearch: true, //菜单是否启用搜索功能
@@ -46,16 +46,17 @@ app.config.globalProperties.$global = {
 }
 //2023.03.13，
 //修改见：volupload.vue，后台AliOSSController.cs，阿里云OSS配置.doc
-window.oss = {
-  ali: {
-    //阿里云
-    use: false, //使用阿里云上传文件
-    //阿里缩略图压缩大小
-    //.aliyuncs.com
-    small: '?x-oss-process=image/resize,m_lfit,w_200'
-    // 填写oss所配置的cdn域名
-    //cdn: "https://static.xxx.com"
-  }
-}
-app.use(store).use(ElementPlus, { size: 'default' }).use(router).use(viewgird).mount('#app')
+// window.oss = {
+//   ali: {
+//     //阿里云
+//     use: false, //使用阿里云上传文件
+//     //阿里缩略图压缩大小
+//     //.aliyuncs.com
+//     small: '?x-oss-process=image/resize,m_lfit,w_200'
+//     // 填写oss所配置的cdn域名
+//     //cdn: "https://static.xxx.com"
+//   }
+// }
+// .use(viewgird)
+app.use(store).use(ElementPlus, { size: 'default' }).use(router).mount('#app')
 app.config.globalProperties.$Message = app.config.globalProperties.$message
--- pek.vuenetcoret.client/src/router/index.ts
+++ pek.vuenetcoret.client/src/router/index.ts
@@ -1,81 +1,81 @@
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
-import form from './form'
-import formsMulti from './formsMulti'
-import tables from './tables'
-import viewgird from './viewGird'
-import store from '../store/index'
-import redirect from './redirect'
-import charts from './charts'
+// import form from './form'
+// import formsMulti from './formsMulti'
+// import tables from './tables'
+// import viewgird from './viewGird'
+import store from '@/store/index'
+// import redirect from './redirect'
+// import charts from './charts'
 const routes = [
-  {
-    path: '/',
-    name: 'Index',
-    component: () => import('@/views/Index'),
-    redirect: '/home',
-    children: [
-      ...form,//Demo表单路由
-      ...formsMulti,
-      ...tables,//tables
-      ...viewgird,
-      ...redirect,
-      ...charts,
-      {
-        path: '/home',
-        name: 'home',
-        component: () => import('@/views/Home.vue')
-      }, {
-        path: '/UserInfo',
-        name: 'UserInfo',
-        component: () => import('@/views/system/UserInfo.vue')
-      },
-      {
-        path: '/sysMenu',
-        name: 'sysMenu',
-        component: () => import('@/views/system/Sys_Menu.vue')
-      }, {
-        path: '/coder',
-        name: 'coder',
-        component: () => import('@/views/builder/coder.vue')
-      },
-      {
-        path: '/formDraggable',  //表单设计
-        name: 'formDraggable',
-        component: () => import('@/views/formDraggable/formDraggable.vue')
-      },
-      {
-        path: '/formSubmit',  //表单提交页面
-        name: 'formSubmit',
-        component: () => import('@/views/formDraggable/FormSubmit.vue'),
-        meta: {
-          keepAlive: false
-        }
-      },
-      {
-        path: '/formCollectionResultTree',  //显示收集的数据表单
-        name: 'formCollectionResultTree',
-        component: () => import('@/views/formDraggable/FormCollectionResultTree.vue'),
-        meta: {
-          keepAlive: false
-        }
-      },
-      {
-        path: '/signalR',
-        name: 'signalR',
-        component: () => import('@/views/signalR/Index.vue'),
-        meta: {
-          keepAlive: false
-        }
-      },
-      {
-        path: '/flowdemo', //发起流程示例
-        name: 'flowdemo',
-        component: () => import('@/views/system/flow/FlowDemo.vue'),
-        meta: {
-          keepAlive: false
-        }
-      }
-    ]
-  },
+  // {
+  //   path: '/',
+  //   name: 'Index',
+  //   component: () => import('@/views/Index'),
+  //   redirect: '/home',
+  //   children: [
+  //     ...form,//Demo表单路由
+  //     ...formsMulti,
+  //     ...tables,//tables
+  //     ...viewgird,
+  //     ...redirect,
+  //     ...charts,
+  //     {
+  //       path: '/home',
+  //       name: 'home',
+  //       component: () => import('@/views/Home.vue')
+  //     }, {
+  //       path: '/UserInfo',
+  //       name: 'UserInfo',
+  //       component: () => import('@/views/system/UserInfo.vue')
+  //     },
+  //     {
+  //       path: '/sysMenu',
+  //       name: 'sysMenu',
+  //       component: () => import('@/views/system/Sys_Menu.vue')
+  //     }, {
+  //       path: '/coder',
+  //       name: 'coder',
+  //       component: () => import('@/views/builder/coder.vue')
+  //     },
+  //     {
+  //       path: '/formDraggable',  //表单设计
+  //       name: 'formDraggable',
+  //       component: () => import('@/views/formDraggable/formDraggable.vue')
+  //     },
+  //     {
+  //       path: '/formSubmit',  //表单提交页面
+  //       name: 'formSubmit',
+  //       component: () => import('@/views/formDraggable/FormSubmit.vue'),
+  //       meta: {
+  //         keepAlive: false
+  //       }
+  //     },
+  //     {
+  //       path: '/formCollectionResultTree',  //显示收集的数据表单
+  //       name: 'formCollectionResultTree',
+  //       component: () => import('@/views/formDraggable/FormCollectionResultTree.vue'),
+  //       meta: {
+  //         keepAlive: false
+  //       }
+  //     },
+  //     {
+  //       path: '/signalR',
+  //       name: 'signalR',
+  //       component: () => import('@/views/signalR/Index.vue'),
+  //       meta: {
+  //         keepAlive: false
+  //       }
+  //     },
+  //     {
+  //       path: '/flowdemo', //发起流程示例
+  //       name: 'flowdemo',
+  //       component: () => import('@/views/system/flow/FlowDemo.vue'),
+  //       meta: {
+  //         keepAlive: false
+  //       }
+  //     }
+  //   ]
+  // },
   {
     path: '/login',
     name: 'login',
@@ -84,22 +84,22 @@ const routes = [
       anonymous: true
     }
   },
-  {
-    path: '/app/guide',
-    name: 'apphome',
-    meta: {
-      anonymous: true
-    },
-    component: () => import('@/views/h5/Guide.vue'),
-  },
-  {
-    path: '/bigdata',
-    name: 'bigdata',
-    component: () => import('@/views/charts/bigdata.vue'),
-    meta: {
-      keepAlive: false
-    }
-  }
+  // {
+  //   path: '/app/guide',
+  //   name: 'apphome',
+  //   meta: {
+  //     anonymous: true
+  //   },
+  //   component: () => import('@/views/h5/Guide.vue'),
+  // },
+  // {
+  //   path: '/bigdata',
+  //   name: 'bigdata',
+  //   component: () => import('@/views/charts/bigdata.vue'),
+  //   meta: {
+  //     keepAlive: false
+  //   }
+  // }
 ]
 
 const router = createRouter({
@@ -109,8 +109,8 @@ const router = createRouter({
 
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.length == 0) return next({ path: '/404' });
-  //2020.06.03增加路由切换时加载提示
+  // if (to.matched.length == 0) return next({ path: '/404' });
+  // //2020.06.03增加路由切换时加载提示
   store.dispatch("onLoading", true);
   if ((to.hasOwnProperty('meta') && to.meta.anonymous) || store.getters.isLogin() || to.path == '/login') {
     return next();
